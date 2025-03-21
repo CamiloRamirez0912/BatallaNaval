56eda0222115a0d01f047a1464197329ddf6c7f9
Camilo Ramirez <camiestebanramirezsalas@gmail.com>
2025-03-20 19:43:44 -0500

    Spring project :)

---
+++ BatallaNaval/src/test/java/co/edu/uptc/BatallaNaval/BatallaNavalApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.BatallaNaval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatallaNavalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
